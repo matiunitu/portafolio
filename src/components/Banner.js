807ee6aba61b770f036933b2c1025acadd5aa409
matiunitu <mathewkel06@gmail.com>
2025-04-02 19:53:39 -0500

    Corrigiendo errs

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import 'animate.css';
@@ -9,22 +9,22 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const toRotate = [ "Web Developer", "Front-End", "Programmer" ];
   const period = 2000;
 
   const tick = useCallback(() => {
+    const toRotate = ["Web Developer", "Front-End", "Programmer"];
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
     let updatedText = isDeleting
       ? fullText.substring(0, text.length - 1)
       : fullText.substring(0, text.length + 1);
-  
+
     setText(updatedText);
-  
+
     if (isDeleting) {
       setDelta(prevDelta => prevDelta / 2);
     }
-  
+
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
       setDelta(period);
@@ -33,7 +33,15 @@ export const Banner = () => {
       setLoopNum(loopNum + 1);
       setDelta(500);
     }
-  }, [loopNum, isDeleting, text, period, toRotate]);
+  }, [loopNum, isDeleting, text]);
+
+  useEffect(() => {
+    const ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => clearInterval(ticker);
+  }, [delta, tick]);
 
   return (
     <section className="banner" id="home">
@@ -42,11 +50,11 @@ export const Banner = () => {
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Bienvenido a Mi Portafolio</span>
-                <h1>{`Hola! Soy Mathew `}<span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Front-End", "Programmer" ]'><span className="wrap">{text}</span></span></h1>
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <span className="tagline">Bienvenido a Mi Portafolio</span>
+                  <h1>{`Hola! Soy Mathew `}<span className="txt-rotate" dataPeriod="1000"><span className="wrap">{text}</span></span></h1>
                   <p>Soy Mathew, un desarrollador front-end con dominio de varios lenguajes. Actualmente, estoy estudiando para ser desarrollador full-stack. Soy responsable, trabajo bien en equipo y llevo tres años en el vasto mundo de la programación.</p>
-              </div>}
+                </div>}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
